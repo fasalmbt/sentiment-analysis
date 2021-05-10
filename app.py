25968b7df2f3298706bc61d59e98864a6a3db77b
fasalmbt <mfmbt786@gmail.com>
2021-05-10 14:07:18 +0530

    :tada: Minor updates

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 import pickle
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, jsonify, request, redirect, render_template
 
 app = Flask(__name__)
 
 vectorizer = pickle.load(open('./models/vectorizer.sav', 'rb'))
 classifier = pickle.load(open('./models/classifier.sav', 'rb'))
+
+@app.route('/')
+def main():
+	return render_template('index.html')
 
 @app.route('/analysis', methods=['POST', 'GET'])
 def sentiment():
@@ -15,6 +19,8 @@ def sentiment():
 			word_vector = vectorizer.transform([word])
 			result = classifier.predict(word_vector)
 			return jsonify({'Sentiment': result[0], 'Word': word, 'Status code': 200})
+		else:
+			return "<center> Please provide a word.</center>"
 		return jsonify({'error':'internal server error', 'Status code': 500})
 
 
